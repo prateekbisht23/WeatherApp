7909f476d05c8780118c947a3b94328b52060a4a
prateekbisht23 <prateekbisht04@gmail.com>
2024-09-26 15:21:23 +0530

    Improved the divs and background design

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
-const Location = () => {
-    return(
-        <>
-            <input type="text" />
-        </>
-    )
+function Location() {
+  return (
+    <div className='border-b-[1px] border-white'>
+        {/* Image of a thermometer */}
+        <input type="text" className='text-white placeholder="Locaion" placeholder-white focus:outline-none bg-transparent'/>
+    </div>
+  )
 }
 
-export default Location;
+export default Location
